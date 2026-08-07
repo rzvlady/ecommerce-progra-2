52b4359866a132f56525b0613729f798da395f6c
Alexander Emestica <alexander.alberto.emestica@gmail.com>
2026-08-06 18:35:02 -0600

    feat(core): inicializar arquitectura base del e-commerce

---
+++ e-commerce/src/test/java/com/sv/grupo1/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sv.grupo1.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
