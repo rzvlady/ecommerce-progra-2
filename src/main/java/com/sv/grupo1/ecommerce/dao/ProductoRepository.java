d2b1c3d56796a059a050a5bffe08772248daf6ba
Brandonmejia19 <brandonmejia887@gmail.com>
2026-09-19 12:14:33 -0600

    Agrega consulta findProductosConStockBajo

--- src/main/java/com/sv/grupo1/ecommerce/dao/ProductoRepository.java
+++ src/main/java/com/sv/grupo1/ecommerce/dao/ProductoRepository.java
@@ -2,7 +2,13 @@ package com.sv.grupo1.ecommerce.dao;
 
 import com.sv.grupo1.ecommerce.entities.core.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
     boolean existsByCodigoSku(String codigoSku);
+
+    @Query("SELECT p FROM Producto p WHERE p.stockMinimo IS NOT NULL AND p.stockDisponible <= p.stockMinimo")
+    List<Producto> findProductosConStockBajo();
 }
