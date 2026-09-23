a3a82489de36e688cd958a4761159dde6de16ab8
kattyfer <katt12ruiz@gmail.com>
2026-09-22 19:54:24 -0600

    actualizando controlador y añadiendo vista de gestion de despachos
    Añadi la estructura base de lista.html para la pestaña de gestion de despacho.

--- src/main/java/com/sv/grupo1/ecommerce/controllers/DespachoController.java
+++ src/main/java/com/sv/grupo1/ecommerce/controllers/DespachoController.java
@@ -1,13 +1,14 @@
 package com.sv.grupo1.ecommerce.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DespachoController{
 
     @GetMapping("/despachos")
-    public String verDespachos(){
-        return"despachoslista";
+    public String verDespachos(Model model){
+        return"admin/despachos/lista";
     }
 }
